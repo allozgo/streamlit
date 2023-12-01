cc73d76380f972f8a36602a3d51c34f4a7492665
allozgo <celamino@hotmail.com>
2023-12-01 13:23:32 +0100

    Streamlit

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ def generate_llama2_response(prompt_input, llma):
     
     if llma == 'stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4':
         st.image(output)
-    return output
+    else:
+        return output
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not replicate_api):
