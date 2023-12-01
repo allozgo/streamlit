ea3081214c5051729586e59e798226b632e4d52b
allozgo <celamino@hotmail.com>
2023-12-01 13:03:53 +0100

    Streamlit

--- app.py
+++ app.py
@@ -58,10 +58,13 @@ def generate_llama2_response(prompt_input, llma):
     output = replicate.run(llma, 
                            input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
                                   "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    '''
     if llma == 'stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4':
         url = output
         response = requests.get(url)
         img = Image.open(BytesIO(response.content))
+        st.image(img)
+        '''
     return output
 
 # User-provided prompt
@@ -83,4 +86,4 @@ if st.session_state.messages[-1]["role"] != "assistant":
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
-    st.image(img, caption='Imagen desde la URL', use_column_width=True)
+    #st.image(img)
